2aea35831e617ad6db952802cec750cbb72ede67
Jan Przybysz <janprzybysz2@gmail.com>
2019-06-01 11:49:23 +0200

    package json

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const notifier = require('node-notifier');
+const c = require('ansi-colors');
+
+function showError(err) {
+    console.log( c.red(err.messageFormatted));
+    notifier.notify({
+        title: 'BŁĄD SASS',
+        message: 'err.messageFormatted'
+    });
+}
+
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+
+gulp.task('sass', function () {
+    return gulp.src('./scss/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: "compressed"  //compact, compressed, nested, expanded
+        }).on('error', showError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+        }))
+        .pipe(sourcemaps.write('.'))  //kropka jako string, nie bedzie komentarza w scss
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream())
+});
+
+gulp.task('watch', function() {
+   gulp.watch('./scss/**/*.scss', ['sass']);
+   gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', function() {
+    console.log('------------');
+    gulp.start(['sass', 'watch', 'browser-sync']);
+});
